feaea2f19d7713a8e65fe05e3a6aa951ca293983
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 02:05:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,18 +1,10 @@
 def Genre
   attr_accessor :name
   
-  @@all = []
-  
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  
-  def songs
-    #return Song.all.select {|x| x.genre == self}
-  end
-  
-  def artists
-  end
+
+end
+
 
 end
