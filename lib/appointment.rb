10c7dc3b3c4f40bb90a1d3199b3f3b1b000e4b0e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 02:16:41 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,9 +2,15 @@ class Appointment
   
   attr_accessor :date, :patient, :doctor
   
+  @@all = []
+  
   def initialize(date, patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
+  end
+  
+  def self.all
+    return @@all
   end
 end
